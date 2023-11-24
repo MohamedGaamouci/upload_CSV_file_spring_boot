7d0aa25d3a01ef5f5664a2efe412a0267b297ff2
MohamedGaamouci <gaamoucimohamed@gmail.com>
2023-11-24 14:31:54 +0100

    upload dependencies

---
+++ src/main/java/com/monsif_team/spring_boot_upload_files/SpringBootUploadFilesApplication.java
@@ -0,0 +1,13 @@
+package com.monsif_team.spring_boot_upload_files;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUploadFilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/monsif_team/spring_boot_upload_files/SpringBootUploadFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monsif_team.spring_boot_upload_files;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUploadFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
